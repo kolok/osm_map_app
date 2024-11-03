ba23f088d91f13549534fda9913230d377ac1b2b
Nicolas Oudard <nicolas@oudard.org>
2024-11-03 07:42:39 +0100

    Afficher une punaise en svg et un panel lors du clique sur la punaise

--- lib/widgets/openstreetmap.dart
+++ lib/widgets/openstreetmap.dart
@@ -5,6 +5,9 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:http/http.dart' as http;
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
+
 import 'plugins/zoombuttons_plugin.dart';
 
 class OpenStreetMap extends StatefulWidget {
@@ -23,6 +26,8 @@ class OpenStreetMap extends StatefulWidget {
 
 class _OpenStreetMapState extends State<OpenStreetMap> {
   List<Marker> _markers = [];
+  String _selectedActorName = '';
+  final PanelController _panelController = PanelController();
 
   @override
   void initState() {
@@ -38,17 +43,31 @@ class _OpenStreetMapState extends State<OpenStreetMap> {
 
   Future<void> _fetchMarkers(double latitude, double longitude) async {
     final response = await http.get(Uri.parse(
-        'https://quefairedemesobjets-preprod.osc-fr1.scalingo.io/api/qfdmo/acteurs?latitude=$latitude&longitude=$longitude'));
+        'https://quefairedemesobjets-preprod.osc-fr1.scalingo.io/api/qfdmo/acteurs?latitude=$latitude&longitude=$longitude&limit=25'));
 
     if (response.statusCode == 200) {
       final data = json.decode(response.body);
       setState(() {
         _markers = (data['items'] as List).map((item) {
           return Marker(
-            width: 80.0,
-            height: 80.0,
+            width: 66.0,
+            height: 81.0,
             point: LatLng(item['latitude'], item['longitude']),
-            child: const Icon(Icons.location_on, color: Colors.red), // Add an empty container as the child
+
+            child: GestureDetector(
+              onTap: () {
+                setState(() {
+                  _selectedActorName =  item['nom']?.toString() ?? 'Nom non disponible';
+                });
+              },
+              child: SvgPicture.asset(
+                'assets/images/recycle_pin.svg',
+                fit: BoxFit.fill,
+                allowDrawingOutsideViewBox: true,
+                width: 66.0, // Ajustez la taille de l'image SVG
+                height: 81.0, // Ajustez la taille de l'image SVG
+              ),
+            ),
           );
         }).toList();
       });
@@ -59,34 +78,65 @@ class _OpenStreetMapState extends State<OpenStreetMap> {
 
   @override
   Widget build(BuildContext context) {
-    return FlutterMap(
-      mapController: widget.mapController,
-      options: MapOptions(
-        initialCenter: widget.initialPosition,
-        initialZoom: 13.0,
-      ),
-      children: [
-        TileLayer(
-          urlTemplate: 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',
-          userAgentPackageName: 'com.example.app',
-        ),
-        MarkerLayer(markers: _markers), // Ajoutez ce widget pour afficher les marqueurs
-        RichAttributionWidget(
-          attributions: [
-            TextSourceAttribution(
-              'OpenStreetMap contributors',
-              onTap: () => launchUrl(Uri.parse('https://openstreetmap.org/copyright')),
+    return Scaffold(
+      body: Stack(
+        children: [
+          FlutterMap(
+            mapController: widget.mapController,
+            options: MapOptions(
+              initialCenter: widget.initialPosition,
+              initialZoom: 13.0,
             ),
-          ],
-        ),
-        FlutterMapZoomButtons(
-          mapController: widget.mapController,
-          minZoom: 4,
-          maxZoom: 19,
-          padding: 10,
-          alignment: Alignment.bottomRight,
-        ),
-      ],
+            children: [
+              TileLayer(
+                urlTemplate: 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',
+                userAgentPackageName: 'com.example.app',
+              ),
+              MarkerLayer(markers: _markers),
+              RichAttributionWidget(
+                attributions: [
+                  TextSourceAttribution(
+                    'OpenStreetMap contributors',
+                    onTap: () => launchUrl(Uri.parse('https://openstreetmap.org/copyright')),
+                  ),
+                ],
+              ),
+              FlutterMapZoomButtons(
+                mapController: widget.mapController,
+                minZoom: 2,
+                maxZoom: 19,
+                padding: 10,
+                alignment: Alignment.bottomRight,
+              ),
+            ],
+          ),
+          if (_selectedActorName.isNotEmpty)
+            SlidingUpPanel(
+              controller: _panelController,
+              minHeight: MediaQuery.of(context).size.height / 3,
+              maxHeight: MediaQuery.of(context).size.height,
+              panel: Center(
+                child: Column(
+                  children: [
+                    IconButton(
+                      icon: Icon(Icons.close),
+                      onPressed: () {
+                        _panelController.close();
+                        setState(() {
+                          _selectedActorName = '';
+                        });
+                      },
+                    ),
+                    Text(
+                      _selectedActorName,
+                      style: TextStyle(fontSize: 24),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+        ],
+      ),
     );
   }
 }
