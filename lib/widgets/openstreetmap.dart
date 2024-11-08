5c52da76a5735dec6289a705898eefbc3f27bb32
Nicolas Oudard <nicolas@oudard.org>
2024-11-08 18:28:35 +0100

    Increase marker size

--- lib/widgets/openstreetmap.dart
+++ lib/widgets/openstreetmap.dart
@@ -27,8 +27,9 @@ class OpenStreetMap extends StatefulWidget {
 }
 
 class OpenStreetMapState extends State<OpenStreetMap> {
-  List<Marker> _markers = [];
+  List<dynamic> _markerData = [];
   String _selectedActorName = '';
+  String? _selectedMarkerId;
   final PanelController _panelController = PanelController();
 
   @override
@@ -46,40 +47,58 @@ class OpenStreetMapState extends State<OpenStreetMap> {
   Future<void> _fetchMarkers(double latitude, double longitude) async {
     final response = await http.get(Uri.parse(
         'https://quefairedemesobjets-preprod.osc-fr1.scalingo.io/api/qfdmo/acteurs?latitude=$latitude&longitude=$longitude&limit=25'));
-
     if (response.statusCode == 200) {
       final data = json.decode(response.body);
       setState(() {
-        _markers = (data['items'] as List).map((item) {
-          return Marker(
-            width: 66.0,
-            height: 81.0,
-            point: LatLng(item['latitude'], item['longitude']),
-
-            child: GestureDetector(
-              onTap: () {
-                setState(() {
-                  _selectedActorName =  item['nom']?.toString() ?? 'Nom non disponible';
-                });
-              },
-              child: SvgPicture.asset(
-                'assets/images/recycle_pin.svg',
-                fit: BoxFit.fill,
-                allowDrawingOutsideViewBox: true,
-                width: 66.0, // Ajustez la taille de l'image SVG
-                height: 81.0, // Ajustez la taille de l'image SVG
-              ),
-            ),
-          );
-        }).toList();
+        _markerData = data['items'] as List;
       });
     } else {
       throw Exception('Failed to load markers');
     }
   }
 
+  List<Marker> _buildMarkers() {
+    return _markerData.map((item) {
+      final markerId = item['identifiant_unique'];
+      final isSelected = _selectedMarkerId == markerId;
+      return Marker(
+        width: isSelected ? 80.0 : 66.0,
+        height: isSelected ? 100.0 : 81.0,
+        point: LatLng(item['latitude'], item['longitude']),
+        child: GestureDetector(
+          onTap: () {
+            setState(() {
+              _selectedActorName = item['nom']?.toString() ?? 'Nom non disponible';
+              _selectedMarkerId = markerId;
+            });
+            // Ouvrir le panneau après la mise à jour de l'état
+            debugPrint('Open panel `$_selectedActorName`');
+            // _panelController.open();
+          },
+          child: SvgPicture.asset(
+            'assets/images/recycle_pin.svg',
+            fit: BoxFit.fill,
+            allowDrawingOutsideViewBox: true,
+          ),
+        ),
+      );
+    }).toList();
+  }
+
+  void _closePanel() {
+    setState(() {
+      _selectedActorName = '';
+      _selectedMarkerId = null;
+    });
+    debugPrint('Close panel');
+    _panelController.close();
+  }
+
   @override
   Widget build(BuildContext context) {
+    // Reconstruire les marqueurs avec la taille mise à jour
+    List<Marker> markers = _buildMarkers();
+
     return Scaffold(
       body: Stack(
         children: [
@@ -94,7 +113,7 @@ class OpenStreetMapState extends State<OpenStreetMap> {
                 urlTemplate: 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',
                 userAgentPackageName: 'com.example.app',
               ),
-              MarkerLayer(markers: _markers),
+              MarkerLayer(markers: markers),
               RichAttributionWidget(
                 attributions: [
                   TextSourceAttribution(
@@ -107,7 +126,11 @@ class OpenStreetMapState extends State<OpenStreetMap> {
                 mapController: widget.mapController,
                 minZoom: 2,
                 maxZoom: 19,
-                padding: 10,
+                padding: 5,
+                zoomInColor: Colors.grey,
+                zoomInColorIcon: Colors.black,
+                zoomOutColor: Colors.grey,
+                zoomOutColorIcon: Colors.black,
                 alignment: Alignment.bottomRight,
               ),
             ],
@@ -119,17 +142,18 @@ class OpenStreetMapState extends State<OpenStreetMap> {
               snapPoint: 0.5,
               maxHeight: MediaQuery.of(context).size.height,
               backdropEnabled: true,
+              // onPanelClosed: _closePanel,
+              // onPanelSlide: (position) {
+              //   if (position < 0.3) {
+              //     _closePanel();
+              //   }
+              // },
               panel: Center(
                 child: Column(
                   children: [
                     IconButton(
                       icon: Icon(Icons.close),
-                      onPressed: () {
-                        _panelController.close();
-                        setState(() {
-                          _selectedActorName = '';
-                        });
-                      },
+                      onPressed: _closePanel,
                     ),
                     Text(
                       _selectedActorName,
